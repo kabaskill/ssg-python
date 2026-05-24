2d3dd2a4d78d85815d091fb6aa09c19012e9a3e8
kabaskill <oguzkabasakal@gmail.com>
2026-05-24 18:26:35 +0200

    create separate repo

---
+++ src/main.py
@@ -0,0 +1,36 @@
+
+import os
+import shutil
+import sys
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+dir_path_static = "./static"
+# dir_path_public = "./public"
+dir_path_public = "../docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+
+def main():
+    base_path = sys.argv[1]
+    if base_path is None:
+        base_path = "/"
+    print("BASEPATH", base_path)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_pages_recursive(
+        dir_path_content,
+        template_path,
+        dir_path_public,
+        base_path
+    )
+
+
+main()
