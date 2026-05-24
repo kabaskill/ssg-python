86a6ebcce736063b1e711fc4b3e23cabf395a6cc
kabaskill <oguzkabasakal@gmail.com>
2026-05-24 18:34:09 +0200

    build website before deploy

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 # dir_path_public = "./public"
-dir_path_public = "../docs"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
